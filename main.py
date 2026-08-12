c1274f8b1191a9044ffbd0c54ae0b769677c0539
Asadbek <asadbek012007@gmail.com>
2026-08-13 00:47:55 +0500

    adding servver

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+import os
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class HealthHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running!")
+
+    def log_message(self, format, *args):
+        pass
+
+def run_server():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), HealthHandler)
+    server.serve_forever()
+
+threading.Thread(target=run_server, daemon=True).start()
+
+
+
+
+
+
 import asyncio
 import logging
 
